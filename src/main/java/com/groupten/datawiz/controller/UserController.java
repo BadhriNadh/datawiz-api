d7379574da5ffdb1c930d5ba65836d00b0817932
BadhriNadh <42695027+BadhriNadh@users.noreply.github.com>
2023-04-03 11:00:08 -0300

    Update UserController.java

--- src/main/java/com/groupten/datawiz/controller/UserController.java
+++ src/main/java/com/groupten/datawiz/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
-public class UserController extends Handler{
+public class UserController /*extends Handler*/{
 
     @Autowired
     UserService userService;
